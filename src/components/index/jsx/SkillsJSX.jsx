e34d9fca040bea51660054f7086cef53c2b2fd7e
eitaar <154091730+eitaar@users.noreply.github.com>
2025-05-31 17:41:14 +0100

    fix: remove unnecessary padding and adjust layout in Interests, Skills, and Stats components

--- src/components/index/jsx/SkillsJSX.jsx
+++ src/components/index/jsx/SkillsJSX.jsx
@@ -1,13 +1,14 @@
 import {Card, CardHeader, CardBody} from "@heroui/react";
 
+
 export default function App(props) {
   return (
     <Card className="w-full h-full flex-1 flex justify-center">
-      <CardHeader className="flex items-center justify-center gap-1 pb-2">
+      <CardHeader className="flex items-center justify-center gap-1">
         <div>{props.skillIcon}</div>
         <p className="text-3xl font-Quantico font-bold text-center">Skills</p>
       </CardHeader>
-      <CardBody className="flex flex-wrap flex-row gap-2 w-full items-start justify-center pt-0">
+      <CardBody className="flex flex-wrap flex-row gap-2 w-full items-start justify-center">
         <div>{props.jsIcon}</div>
         <div>{props.tsIcon}</div>
         <div>{props.nuxtIcon}</div>

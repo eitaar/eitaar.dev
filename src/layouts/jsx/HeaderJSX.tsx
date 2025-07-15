62f419f51b11dd1e013f0d803cfa5258c5396690
eitaar <154091730+eitaar@users.noreply.github.com>
2025-07-15 20:52:25 +0900

    verion n (n = where I wiped my program)

--- src/layouts/jsx/HeaderJSX.tsx
+++ src/layouts/jsx/HeaderJSX.tsx
@@ -1,8 +1,6 @@
 import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@heroui/react";
 import { gsap } from "gsap";
 import { useState, useEffect } from "react";
-import Title from "../../components/index/astro/Title.astro";
-    
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { ScrollSmoother } from "gsap/ScrollSmoother";
 import { ScrollToPlugin } from "gsap/ScrollToPlugin";
@@ -46,38 +44,7 @@ export default function App(props:any) {
           eitaar.dev
         </Link>
       </NavbarBrand>
-      {
-        <NavbarContent className="hidden sm:flex gap-4" justify="center">        
-          <NavbarItem>
-            <Link
-            className="cursor-pointer"
-            color="primary" 
-            underline="hover"
-            onPressEnd={()=>{
-              const aboutElement = document.getElementById("ABOUT");
-              gsap.to(window, { duration: 0.2, scrollTo: {y:'#ABOUT', 
-                offsetY : aboutElement ? aboutElement.offsetHeight : 0,
-              }});
-            }}
-            >
-              About
-            </Link>
-          </NavbarItem>
-          <NavbarItem>
-            <Link 
-            color="primary"
-            className="cursor-pointer"
-            underline="hover"
-            onPressEnd={()=>{
-              const projectsElement = document.getElementById("PROJECTS");
-              gsap.to(window, { duration: 0.2, scrollTo: {y:'#PROJECTS', 
-                offsetY : projectsElement ? projectsElement.offsetHeight : 0,
-              }});
-            }}
-            >
-              Projects
-            </Link>
-          </NavbarItem>
+        <NavbarContent justify="center">
           <NavbarItem>
             <Link 
               color="primary"
@@ -89,7 +56,6 @@ export default function App(props:any) {
             </Link>
           </NavbarItem>
         </NavbarContent>
-      }
       <NavbarContent justify="end">        
         <NavbarItem>
           <Button

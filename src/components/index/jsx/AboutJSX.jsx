14024cb44520c97781c2f4c718bf3444a911bae7
eitaar <154091730+eitaar@users.noreply.github.com>
2025-07-15 20:45:43 +0900

    I love colourfull page d=====(￣▽￣*)b

--- src/components/index/jsx/AboutJSX.jsx
+++ src/components/index/jsx/AboutJSX.jsx
@@ -1,35 +1,36 @@
 import { Chip, Button } from "@heroui/react";
 export default function App(props) {
   return (
-    <div className="flex gap-3 max-w-1/2">
-      <div>
+    <div className="flex items-center justify-center gap-5 lg:max-w-2/5 h-full mx-auto">
+      <div className="flex gap-3 flex-row items-center">
         {props.avatarImg}
-      </div>
-      <div className="flex-1 p-4">
-        <h2 className="text-4xl font-bold font-Quantico tracking-tight text-success-500">eitaar</h2>
-        <div className="flex flex-row gap-0">
-          <Button
-            isIconOnly={true}
-            variant="light"
-          >
-            {props.githubIcon}
-          </Button>
-          <Button
-            isIconOnly={true}
-            variant="light"
-          >
-            {props.xIcon}
-          </Button>
-          <Button
-            isIconOnly={true}
-            variant="light"
-          >
-            {props.discordIcon}
-          </Button>
+        <div className="flex items-center flex-col gap-1">
+          <h2 className="text-5xl font-bold font-Quantico text-success-500 tracking-tighter">eitaar</h2>
+          <Chip
+            variant="flat"
+            color="primary"
+            >Japanese | 16</Chip>
+          <div className="flex flex-row gap-0">
+            <Button
+              isIconOnly={true}
+              variant="light"
+            >
+              {props.githubIcon}
+            </Button>
+            <Button
+              isIconOnly={true}
+              variant="light"
+            >
+              {props.xIcon}
+            </Button>
+            <Button
+              isIconOnly={true}
+              variant="light"
+            >
+              {props.discordIcon}
+            </Button>
+          </div>
         </div>
-        <p className="font-Quantico tracking-tight px-2">
-          
-        </p>
       </div>
     </div>
   );

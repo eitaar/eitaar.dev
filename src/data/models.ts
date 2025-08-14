c780185bf1160c23034a58feebad5000e7cdc7d0
eitaar <154091730+eitaar@users.noreply.github.com>
2025-08-14 16:30:30 +0900

    feat: added models

--- src/data/models.ts
+++ src/data/models.ts
@@ -16,6 +16,22 @@ const models: Model[] = [
     software: 'Blockbench',
     polygons: 397,
   },
+  {
+    filename: 'gun.gltf',
+    title: 'Revolver',
+    description: 'A model of a revolver',
+    date: new Date('2023-05-03'),
+    software: 'Blockbench',
+    polygons: 50,
+  },
+  {
+    filename: 'msk.gltf',
+    title: 'Insect Cage',
+    description: 'A model of a cage for insects',
+    date: new Date('2024-08-21'),
+    polygons: 84,
+    software: 'Blockbench',
+  },
 ];
 
 export default models;

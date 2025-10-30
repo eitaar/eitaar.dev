1ef1dde2ac4ef4deb5a0e042b8b19a2ff54c6911
eitaar <154091730+eitaar@users.noreply.github.com>
2025-10-30 09:46:42 +0000

    feat: add ProjectCard component and project data
    - Implemented ProjectCard component to display project details including title, description, and image.
    - Created projects data file with sample project entries for use in the ProjectCard component.

---
+++ src/data/projects.ts
@@ -0,0 +1,29 @@
+import portFolioImg from "../assets/img/projects/portfolio.png";
+import type { ProjectProps } from "../types/global";
+
+export const Projects: ProjectProps[] = [
+	{
+		title: "Portfolio",
+		description:
+			"A personal portfolio website to showcase my projects and skills, built with Astro and Tailwind CSS.",
+		imageUrl: portFolioImg,
+		repoUrl: "https://github.com/eitaar/eitaar.dev",
+		tags: ["Astro", "Tailwind CSS", "Web"],
+	},
+	{
+		title: "Portfolio",
+		description:
+			"A personal portfolio website to showcase my projects and skills, built with Astro and Tailwind CSS.",
+		imageUrl: portFolioImg,
+		repoUrl: "https://github.com/eitaar/eitaar.dev",
+		tags: ["Astro", "Tailwind CSS", "Web"],
+	},
+	{
+		title: "Portfolio",
+		description:
+			"A personal portfolio website to showcase my projects and skills, built with Astro and Tailwind CSS.",
+		imageUrl: portFolioImg,
+		repoUrl: "https://github.com/eitaar/eitaar.dev",
+		tags: ["Astro", "Tailwind CSS", "Web"],
+	},
+];
